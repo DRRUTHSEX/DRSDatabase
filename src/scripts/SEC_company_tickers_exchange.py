291e25040d9b79d192d698a3407d02ccda98f0e1
DRRUTHSEX <121346437+DRRUTHSEX@users.noreply.github.com>
2024-06-12 23:58:21 -0400

    Update SEC_company_tickers_exchange.py

--- src/scripts/SEC_company_tickers_exchange.py
+++ src/scripts/SEC_company_tickers_exchange.py
@@ -13,7 +13,11 @@ output_file = os.path.join(data_folder, "company_tickers_exchange.json")
 
 # Set headers to mimic a browser request
 headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3",
+    "Accept-Language": "en-US,en;q=0.9",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+    "Connection": "keep-alive"
 }
 
 # Download the JSON file
