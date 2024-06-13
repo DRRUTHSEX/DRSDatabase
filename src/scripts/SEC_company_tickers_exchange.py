5f7ead9f367dea4555523a72f53c59a0f5960172
DRRUTHSEX <121346437+DRRUTHSEX@users.noreply.github.com>
2024-06-12 23:53:50 -0400

    Update SEC_company_tickers_exchange.py

--- src/scripts/SEC_company_tickers_exchange.py
+++ src/scripts/SEC_company_tickers_exchange.py
@@ -11,8 +11,13 @@ os.makedirs(data_folder, exist_ok=True)
 # Path to the output file
 output_file = os.path.join(data_folder, "company_tickers_exchange.json")
 
+# Set headers to mimic a browser request
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
+}
+
 # Download the JSON file
-response = requests.get(url)
+response = requests.get(url, headers=headers)
 response.raise_for_status()  # Check that the request was successful
 
 # Save the JSON file
