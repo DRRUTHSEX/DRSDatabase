9829c493d53701e44f1b562d27dc30145fccefe3
DRRUTHSEX <121346437+DRRUTHSEX@users.noreply.github.com>
2024-06-13 00:26:35 -0400

    Update json_to_csv.py

--- src/scripts/json_to_csv.py
+++ src/scripts/json_to_csv.py
@@ -10,20 +10,12 @@ csv_file_path = 'data/company_tickers_exchange.csv'
 with open(json_file_path, 'r') as json_file:
     data = json.load(json_file)
 
-# Check the structure of the JSON data
-print(type(data))
-print(data.keys())
+# Extract fields and data
+fields = data['fields']
+records = data['data']
 
-# Assuming the JSON data is a dictionary where the values need to be flattened
-# Flatten the JSON data for conversion
-flattened_data = []
-
-for key, value in data.items():
-    value['symbol'] = key  # Add the key (symbol) to the dictionary
-    flattened_data.append(value)
-
-# Convert the flattened data to DataFrame
-df = pd.DataFrame(flattened_data)
+# Convert to DataFrame
+df = pd.DataFrame(records, columns=fields)
 
 # Save DataFrame to CSV
 df.to_csv(csv_file_path, index=False)
