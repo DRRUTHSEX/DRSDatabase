1a126310540cd40ef364614f957ca98b01f2b661
DRRUTHSEX <121346437+DRRUTHSEX@users.noreply.github.com>
2024-09-17 21:23:51 -0400

    Add files via upload

--- src/web/script.js
+++ src/web/script.js
@@ -1,7 +1,7 @@
 // Assuming your JSON file is named 'Full_Database_Backend.json' and is in the same directory
 document.addEventListener("DOMContentLoaded", function() {
     const loadingBar = document.getElementById('loading-bar');
-    const dataTable = document.getElementById('data-table');
+    const dataTableElement = document.getElementById('data-table');
 
     // Show the loading bar
     loadingBar.style.display = 'block';
@@ -38,13 +38,39 @@ document.addEventListener("DOMContentLoaded", function() {
                     table.tBodies[0].appendChild(row); // Appends the row to the table body
                 });
 
+                // Create columns array for DataTables
+                const columns = headers.map(header => ({ title: header }));
+
+                // Determine which columns should be visible by default
+                // Columns 1,2,3,4,9,10 are visible (indices 0,1,2,3,8,9)
+                const visibleColumns = [0,1,2,3,8,9];
+
                 // Initialize DataTables
                 $(document).ready(function() {
                     $('#data-table').DataTable({
+                        columns: columns,
+                        "columnDefs": [
+                            {
+                                "targets": "_all",
+                                "visible": false
+                            },
+                            {
+                                "targets": visibleColumns,
+                                "visible": true
+                            }
+                        ],
+                        dom: 'Bfrtip',
+                        buttons: [
+                            {
+                                extend: 'colvis',
+                                columns: ':gt(0)',
+                                text: 'Select Columns'
+                            }
+                        ],
                         "initComplete": function(settings, json) {
                             // Hide the loading bar and show the table after DataTables initialization is complete
                             loadingBar.style.display = 'none';
-                            dataTable.style.display = 'table';
+                            dataTableElement.style.display = 'table';
                         },
                         "pagingType": "simple", // Simplifies pagination controls
                         "language": {
