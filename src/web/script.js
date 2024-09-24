4db6891ba5fc07a266ecc6e130aa4acbd00c4013
DRRUTHSEX <121346437+DRRUTHSEX@users.noreply.github.com>
2024-09-24 02:11:49 -0400

    Update script.js

--- src/web/script.js
+++ src/web/script.js
@@ -55,18 +55,19 @@ document.addEventListener("DOMContentLoaded", function () {
 
                                     // Redraw the table without resetting the paging
                                     dt.draw(false);
+
+                                    // Adjust columns and recalculate responsiveness
+                                    dt.columns.adjust().responsive.recalc();
                                 }
                             }
                         ],
                         "stateSave": true,
                         "stateDuration": -1, // Set to -1 to save the state indefinitely
-                        // In initComplete function:
                         "initComplete": function (settings, json) {
-                        // Hide the loading overlay and show the table after DataTables initialization is complete
+                            // Hide the loading overlay and show the table after DataTables initialization is complete
                             loadingOverlay.style.display = 'none';
                             dataTableElement.style.display = 'table';
                         },
-
                         "pagingType": "full_numbers", // Displays page numbers
                         "language": {
                             "search": "",
@@ -79,6 +80,11 @@ document.addEventListener("DOMContentLoaded", function () {
                         ],
                         "order": [[0, 'asc']], // Sort by the first column (index 0) ascending
                         "responsive": true // Enable responsive table
+                    });
+
+                    // Add event listener for column visibility change
+                    table.on('column-visibility.dt', function (e, settings, column, state) {
+                        table.columns.adjust().responsive.recalc();
                     });
                 });
             })
