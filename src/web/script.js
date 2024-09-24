9e7180f06686a392f374256baf731cac5af2b08b
DRRUTHSEX <121346437+DRRUTHSEX@users.noreply.github.com>
2024-09-24 02:45:37 -0400

    changes

--- src/web/script.js
+++ src/web/script.js
@@ -55,21 +55,16 @@ document.addEventListener("DOMContentLoaded", function () {
 
                                     // Redraw the table without resetting the paging
                                     dt.draw(false);
-
-                                    // Adjust columns and redraw
-                                    dt.columns.adjust().draw(false);
                                 }
                             }
                         ],
                         "stateSave": true,
                         "stateDuration": -1, // Set to -1 to save the state indefinitely
+                        // In initComplete function:
                         "initComplete": function (settings, json) {
                             // Hide the loading overlay and show the table after DataTables initialization is complete
                             loadingOverlay.style.display = 'none';
                             dataTableElement.style.display = 'table';
-
-                            // Adjust columns
-                            this.api().columns.adjust();
                         },
                         "pagingType": "full_numbers", // Displays page numbers
                         "language": {
@@ -81,17 +76,8 @@ document.addEventListener("DOMContentLoaded", function () {
                             [100, 200, 300, 400, 500, 600, 700, 800, 900, 1000],
                             [100, 200, 300, 400, 500, 600, 700, 800, 900, 1000]
                         ],
-                        "order": [[0, 'asc']], // Sort by the first column (index 0) ascending
-                        "autoWidth": false,
-                        "scrollX": true, // Enable horizontal scrolling
-                        "responsive": false // Disable responsive behavior
+                        "order": [[0, 'asc']] // Sort by the first column (index 0) ascending
                     });
-
-                    // Add event listener for column visibility changes
-                    table.on('column-visibility.dt', function (e, settings, column, state) {
-                        table.columns.adjust().draw(false);
-                    });
-
                 });
             })
             .catch(error => {
