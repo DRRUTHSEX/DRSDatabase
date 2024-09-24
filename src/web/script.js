ea2b061dccf0ef71f422c0b4e552183d70655d75
DRRUTHSEX <121346437+DRRUTHSEX@users.noreply.github.com>
2024-09-24 03:03:56 -0400

    changes

--- src/web/script.js
+++ src/web/script.js
@@ -59,13 +59,13 @@ document.addEventListener("DOMContentLoaded", function () {
                             }
                         ],
                         "stateSave": true,
-                        "stateDuration": -1, // Set to -1 to save the state indefinitely
+                        "stateDuration": -1,
                         "initComplete": function (settings, json) {
                             // Hide the loading overlay and show the table after DataTables initialization is complete
                             loadingOverlay.style.display = 'none';
                             dataTableElement.style.display = 'table';
                         },
-                        "pagingType": "full_numbers", // Displays page numbers
+                        "pagingType": "full_numbers",
                         "language": {
                             "search": "",
                             "searchPlaceholder": "Search records"
@@ -75,8 +75,8 @@ document.addEventListener("DOMContentLoaded", function () {
                             [100, 200, 300, 400, 500, 600, 700, 800, 900, 1000],
                             [100, 200, 300, 400, 500, 600, 700, 800, 900, 1000]
                         ],
-                        "order": [[0, 'asc']], // Sort by the first column (index 0) ascending
-                        "autoWidth": true // Enable automatic column width calculation
+                        "order": [[0, 'asc']],
+                        "autoWidth": false // Disable automatic column width calculation
                     });
 
                     // Event listener to adjust columns when visibility changes
