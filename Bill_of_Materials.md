7962c01b9d09bba19de5eab631d924126612e4fd
DRRUTHSEX <121346437+DRRUTHSEX@users.noreply.github.com>
2024-11-03 14:49:18 -0500

    Testing

---
+++ Bill_of_Materials.md
@@ -0,0 +1,2 @@
+Insert BoM here
+This includes progrmaming languages, libraries, fonts, licenses, etc. that are used in this repository.
--- src/web/react/LoadingBar.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const LoadingBar = ({ loading }) => {
-    return loading ? <div id="loading-bar">Loading...</div> : null;
-};
-
-export default LoadingBar;
